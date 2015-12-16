e545ca310d602abd580964adeb1df9caa5b200b0
minzatmarius <minzat.marius94@yahoo.com>
2015-12-16 17:00:13 +0200

    Two goats will eat 10kg in 5 days

--- Capre/CapreTest.cs
+++ Capre/CapreTest.cs
@@ -12,6 +12,13 @@ namespace Capre
             float Kg = GetKg(1, 1, 1, 1, 1);
             Assert.AreEqual(1, Kg);
         }
+        [TestMethod]
+        public void TwoGoatsInFiveDays()
+        {
+            float Kg = GetKg(1, 1, 1, 5, 2);
+            Assert.AreEqual(10, Kg);
+
+        }
 
         float GetKg(int xDays, int yGoats, float zKg, int wDays, int qGoats)
         {
