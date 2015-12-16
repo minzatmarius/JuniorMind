59110a8297b1e643f6cfa7e68eb7d08b84049e9e
minzatmarius <minzat.marius94@yahoo.com>
2015-12-16 18:47:37 +0200

    He needs 4 repetitions in two rounds
    1 + 2 + 1

---
+++ Sportiv/SportivTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sportiv
+{
+    [TestClass]
+    public class SportivTest
+    {
+        [TestMethod]
+        public void RepetitionsInTwoRounds()
+        {
+            int repetitions = GetRepetitions(2);
+            Assert.AreEqual(4, repetitions);
+        }
+
+        int GetRepetitions(int n)
+        {   
+
+
+            return n+((n-1)*n);
+        }
+    }
+}
