ea355a3cc8550857ac07b8b10748d288b472ecb1
minzatmarius <minzat.marius94@yahoo.com>
2015-12-16 18:21:23 +0200

    For a 3x4 market, 12 1x1 cubes are required

--- Pavaj/PavajTest.cs
+++ Pavaj/PavajTest.cs
@@ -12,6 +12,12 @@ namespace Pavaj
             int cubes = GetNumberOfCubes(2, 2, 1);
             Assert.AreEqual(4, cubes);
         }
+        [TestMethod]
+        public void CubesNeededForAThreeByFour()
+        {
+            int cubes = GetNumberOfCubes(3, 4, 1);
+            Assert.AreEqual(12, cubes);
+        }
 
         int GetNumberOfCubes(int m, int n, int a) {
             if (m % a != 0)
