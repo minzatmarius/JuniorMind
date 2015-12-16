5fccef63447a0ddfc89c60bd7028c72ecc727f9c
minzatmarius <minzat.marius94@yahoo.com>
2015-12-16 18:18:48 +0200

    For a 2x2 market, 4 1x1 cubes are required

---
+++ Pavaj/PavajTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pavaj
+{
+    [TestClass]
+    public class PavajTest
+    {
+        [TestMethod]
+        public void CubesNeededForATwoByTwoMarket()
+        {
+            int cubes = GetNumberOfCubes(2, 2, 1);
+            Assert.AreEqual(4, cubes);
+        }
+
+        int GetNumberOfCubes(int m, int n, int a) {
+            if (m % a != 0)
+                { m += a; }
+            if (n % a != 0)
+                { n += a; }
+
+            return (m * n) / a;
+        }
+    }
+}
