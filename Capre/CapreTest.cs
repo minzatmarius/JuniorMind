22d70e4da001f8e73a0a0b7b29e2da78471370ef
minzatmarius <minzat.marius94@yahoo.com>
2015-12-16 16:48:09 +0200

    Capre empty

---
+++ Capre/CapreTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capre
+{
+    [TestClass]
+    public class CapreTest
+    {
+        [TestMethod]
+        public void OneGoatInOneDay()
+        {
+            int Kg = GetKg(1, 1, 1, 1, 1);
+            Assert.AreEqual(1, Kg);
+        }
+
+        int GetKg(int x, int y, int z, int w, int q)
+        {
+            return 0;
+        }
+
+    }
+}
