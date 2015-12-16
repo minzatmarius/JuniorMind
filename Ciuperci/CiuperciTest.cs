aec6a57ec05e2cddbd018a117b5510948e3f484f
minzatmarius <minzat.marius94@yahoo.com>
2015-12-16 16:15:24 +0200

    Calculate number of red mushrooms

---
+++ Ciuperci/CiuperciTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Ciuperci
+{
+    [TestClass]
+    public class CiuperciTest
+    {
+        [TestMethod]
+        public void NumberOfRedMushrooms()
+        {
+            int red = GetRedMushrooms(10, 4);
+            Assert.AreEqual(8, red);
+        }
+
+        int GetRedMushrooms(int n, int x)
+        {
+            return n - (n / (x + 1));
+        }
+
+    }
+}
