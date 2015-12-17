ea95b219df12bc164180ad1c5b1ea012e8094252
minzatmarius <minzat.marius94@yahoo.com>
2015-12-17 15:32:49 +0200

    For a 2x1 room, 3 whole boards are required

---
+++ Parchet/ParchetTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parchet
+{
+    [TestClass]
+    public class ParchetTest
+    {
+        [TestMethod]
+        public void NumberOfBoardsForATwoByOneRoom()
+        {
+            float boards = GetNumberOfBoards(2, 1, 1, 1);
+            Assert.AreEqual(3, boards);
+        }
+
+
+
+        float GetNumberOfBoards(int n,int m,int a,int b)
+        {
+       
+            return (float) Math.Ceiling(((m / b) * (n / a)) + ((m / b) * (n / a))*0.15f);
+        }
+    }
+}
