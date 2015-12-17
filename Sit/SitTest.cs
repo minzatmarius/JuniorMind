b299892c6037281e34d5ad9757fb843b153450b2
minzatmarius <minzat.marius94@yahoo.com>
2015-12-17 16:19:49 +0200

    Area of a triangle A(1,1) B(3,1) C(3,3)

---
+++ Sit/SitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sit
+{
+    [TestClass]
+    public class SitTest
+    {
+        [TestMethod]
+        public void Area()
+        {
+            float area = GetArea(1, 1, 3, 1, 3, 3);
+            Assert.AreEqual(2, area);
+        }
+
+        float GetArea(int x1,int y1,int x2, int y2, int x3, int y3)
+        {
+            float delta = (x1 * y2) + (x2 * y3) + (y1 *x3) - (x3 * y2) - (y3 * x1) - (x2 * y1);
+            float area = (Math.Abs(delta)) / 2;
+            return area;
+        }
+    }
+}
