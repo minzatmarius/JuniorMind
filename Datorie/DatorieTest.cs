642dd1fc01abacc202ae6798206eab5814ffdeda
minzatmarius <minzat.marius94@yahoo.com>
2015-12-21 12:24:20 +0200

    If you are 8 days late, you' ll have to pay 2% penalty

---
+++ Datorie/DatorieTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Datorie
+{
+    [TestClass]
+    public class DatorieTest
+    {
+        [TestMethod]
+        public void EightDaysLate()
+        {
+            decimal sum = GetTotalSum(100, 8);
+            Assert.AreEqual(102, sum); 
+        }
+
+        decimal GetTotalSum(int rent, int daysLate)
+        {
+            decimal penalty=0;
+
+            if (daysLate > 30)
+            {
+                penalty = rent * 0.1m;
+            }
+            if ((daysLate > 10) && (daysLate < 31))
+            {
+                penalty = rent * 0.05m;
+            }
+            if ((daysLate > 0) && (daysLate < 11))
+            {
+                penalty = rent * 0.02m;
+            }
+            return rent + penalty;
+        }
+    }
+}
