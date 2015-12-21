eaa757636d7306e415350053e0f2ae189d0dfaa3
minzatmarius <minzat.marius94@yahoo.com>
2015-12-21 11:58:57 +0200

    Testing a Fizz type of number

---
+++ FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void TestFizz()
+        {
+            string answer = GetType(9);
+            Assert.AreEqual("Fizz", answer);
+
+        }
+
+        string GetType(int number)
+        {
+            if (number % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return "Not a multiple!";
+        }
+    }
+}
