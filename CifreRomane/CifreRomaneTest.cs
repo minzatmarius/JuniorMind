97c90fa58c9390a940336a4acdc4e20af232695a
minzatmarius <minzat.marius94@yahoo.com>
2015-12-30 17:38:56 +0200

    Taxi empty

---
+++ CifreRomane/CifreRomaneTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CifreRomane
+{
+    [TestClass]
+    public class CifreRomaneTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string result = Convert(1);
+            Assert.AreEqual("I", result);
+
+        }
+
+        string Convert(int number)
+        {
+            return "nothing";
+        }
+    }
+}
---
+++ Taxi/TaxiTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Taxi
+{
+    [TestClass]
+    public class TaxiTest
+    {
+        [TestMethod]
+        public void ShortDistanceDay()
+        {
+
+        }
+
+        decimal GetPrice(int distance, int time)
+        {
+            return 0;
+        }
+    }
+}
