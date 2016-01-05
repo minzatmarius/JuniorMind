772ff445e1bd0ab157597c0600baff5736aaa6b9
minzatmarius <minzat.marius94@yahoo.com>
2016-01-05 16:18:55 +0200

    Roman empty

---
+++ Roman/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Roman
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckForOne()
+        {
+            string result = ToRoman(1);
+            Assert.AreEqual("i", result); 
+        }
+
+        string ToRoman(int number)
+        {
+
+            return "";
+        }
+    }
+}
