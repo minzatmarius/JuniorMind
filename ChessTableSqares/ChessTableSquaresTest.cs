7aac5b1268f900d883445111387a7801e5f3cc53
minzatmarius <minzat.marius94@yahoo.com>
2016-01-07 13:06:48 +0200

    ChessTableSquares empty

---
+++ ChessTableSqares/ChessTableSquaresTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessTableSqares
+{
+    [TestClass]
+    public class ChessTableSquaresTest
+    {
+        [TestMethod]
+        public void OneByOneTable()
+        {
+            int squares = GetSquaresNeeded(1);
+            Assert.AreEqual(1, squares);
+        }
+
+        int GetSquaresNeeded(int tableSize)
+        {
+            return 0;
+        }
+    }
+   
+}
