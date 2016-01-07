4ed92b898ae22efbcd188cec549402d91bb7ed33
minzatmarius <minzat.marius94@yahoo.com>
2016-01-07 13:33:28 +0200

    Prefix empty

---
+++ Prefix/PrefixTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prefix
+{
+    [TestClass]
+    public class PrefixTest
+    {
+        [TestMethod]
+        public void CheckForOneCharacter()
+        {
+            string prefix = GetPrefix("abc", "acb");
+            Assert.AreEqual("a", prefix);
+        }
+
+        string GetPrefix(string firstString, string secondString)
+        {
+            string prefix = "";
+            return prefix;
+        }
+    }
+}
