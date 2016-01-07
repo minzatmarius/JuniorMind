9356069476fc329f107f87dc7819e4813d8e8cad
minzatmarius <minzat.marius94@yahoo.com>
2016-01-07 14:17:24 +0200

    Panagram empty

---
+++ Panagram/PanagramTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Panagram
+{
+    [TestClass]
+    public class PanagramTest
+    {
+        [TestMethod]
+        public void ThePhraseHasOneCharacter()
+        {
+            bool result = IsPanagram("a");
+            Assert.AreEqual(true, result);
+        }
+
+        bool IsPanagram(string phrase)
+        {
+
+
+            return false;
+        }
+    }
+}
