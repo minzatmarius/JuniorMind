6800b82eb68b335d05b4008bf6431e0342e11fec
minzatmarius <minzat.marius94@yahoo.com>
2016-01-08 12:19:02 +0200

    Anagram empty

---
+++ Anagram/AnagramTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagram
+{
+    [TestClass]
+    public class AnagramTest
+    {
+        [TestMethod]
+        public void OneLetterWord()   
+        {
+            int possible = GetPossible("a");
+            Assert.AreEqual(1, possible);
+        }
+
+        int GetPossible(string word)
+        {
+            return 0;
+        }
+    }
+}
