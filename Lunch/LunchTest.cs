4588b002b2d6abaf508e117e458c1f7201570bd7
minzatmarius <minzat.marius94@yahoo.com>
2016-01-08 12:36:08 +0200

    Lunch empty

---
+++ Lunch/LunchTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lunch
+{
+    [TestClass]
+    public class LunchTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        int GetDay(int first, int second)
+        {
+            return 0;
+        }
+    }
+}
