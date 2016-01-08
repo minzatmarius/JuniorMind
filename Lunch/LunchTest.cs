f74bc2e808a3830e2fd64390ccc2fd662d49c271
minzatmarius <minzat.marius94@yahoo.com>
2016-01-08 17:41:05 +0200

    Test passed if the first one goes every fourth day and the second one goes every sixth day
    If they met today (day 1), they will meet again on day 13

--- Lunch/LunchTest.cs
+++ Lunch/LunchTest.cs
@@ -12,12 +12,28 @@ namespace Lunch
             int meetingDay = GetMeetingDay(1, 1);
             Assert.AreEqual(2, meetingDay);
         }
-
+        [TestMethod]
+        public void IfOneGoesEveryFourDaysAndTheOtherEverySixDays()
+        {
+            int meetingDay = GetMeetingDay(4, 6);
+            Assert.AreEqual(13, meetingDay);
+        }
         int GetMeetingDay(int first, int second)
         {
             int currentDay = 1;
+            
 
-            return currentDay + first;
+            for (int i=1; ; i ++)
+            {
+                currentDay++;
+                if(i % first == 0 && i % second == 0)
+                {
+                    break;
+                }
+                
+            }
+
+            return currentDay;
         }
     }
 }
