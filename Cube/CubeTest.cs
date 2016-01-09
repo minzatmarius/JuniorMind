3adeb6fe23912e0df772bb4b2deaa8e29f39b9e3
minzatmarius <minzat.marius94@yahoo.com>
2016-01-09 18:22:56 +0200

    Cube empty

---
+++ Cube/CubeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cube
+{
+    [TestClass]
+    public class CubeTest
+    {
+        [TestMethod]
+        public void FirstNumberShouldBe192()
+        {
+            int number = GetNumber(1);
+            Assert.AreEqual(192, number);
+        }
+
+        bool IsOK(int number)
+        {
+            return false;
+        }
+
+        int GetNumber(int k)
+        {
+            return 0;
+        }
+    }
+}
