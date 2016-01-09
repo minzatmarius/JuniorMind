f1177d0048c27d55f102dd90738257e5c11ceabc
minzatmarius <minzat.marius94@yahoo.com>
2016-01-09 17:59:36 +0200

    Test passed for 5/49

--- Loto/LotoTest.cs
+++ Loto/LotoTest.cs
@@ -9,26 +9,26 @@ namespace Loto
         [TestMethod]
         public void ChancesToGetOneNumberRightOutOFFive()
         {
-            double chance = GetChance(1, 5);
+            double chance = GetChance(1, 1, 5);
             Assert.AreEqual(0.2, chance);
         }
         [TestMethod]
         public void ChancesToGetTwoNumbersRightOutOFFive()
         {
-            double chance = GetChance(2, 5);
+            double chance = GetChance(2, 2, 5);
             Assert.AreEqual(0.1, chance);
         }
         [TestMethod]
         public void ChancesToGet6NumbersRightOutOF49()
         {
-            double chance = GetChance(6, 49);
+            double chance = GetChance(6, 6, 49);
             Assert.AreEqual(1.0/ 13983816, chance, 0.000000000000000001);
         }
         [TestMethod]
         public void ChancesToGet5NumbersRightOutOF49()
         {
-            double chance = GetChance(5, 49);
-            Assert.AreEqual(1.0 / 2330636, chance);
+            double chance = GetChance(5, 6, 49);
+            Assert.AreEqual(0.0000184, chance, 0.0000001);
         }
 
         double Factorial(int number)
@@ -47,9 +47,11 @@ namespace Loto
             return Factorial(n) / (Factorial(k) * Factorial(n - k));
         }
 
-        double GetChance(int chosenNumbers, int totalNumbers)
+        double GetChance(int winnerNumbers, int chosenNumbers, int totalNumbers)
         {
-            double chances = 1 / (Combinations(totalNumbers, chosenNumbers));
+
+            //double chances = 1 / (Combinations(totalNumbers, chosenNumbers));
+            double chances = (Combinations(chosenNumbers, winnerNumbers) * Combinations(totalNumbers - chosenNumbers, chosenNumbers - winnerNumbers)) / Combinations(totalNumbers, chosenNumbers);
            
             return chances; 
            
