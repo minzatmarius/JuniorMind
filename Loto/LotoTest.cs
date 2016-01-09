c62f5de50e69e50df4815c0398a907f67a85db50
minzatmarius <minzat.marius94@yahoo.com>
2016-01-09 15:10:55 +0200

    Test passed for (2, 5)

--- Loto/LotoTest.cs
+++ Loto/LotoTest.cs
@@ -12,10 +12,22 @@ namespace Loto
             double chance = GetChance(1, 5);
             Assert.AreEqual(0.2, chance);
         }
+        [TestMethod]
+        public void ChancesToGetTwoNumbersRightOutOFFive()
+        {
+            double chance = GetChance(2, 5);
+            Assert.AreEqual(0.1, chance);
+        }
 
         double GetChance(int chosenNumbers, int totalNumbers)
         {
+            if(chosenNumbers > 1)
+            {
+                return (chosenNumbers / 1.0 / totalNumbers) * ((chosenNumbers - 1) / 1.0 / (totalNumbers - 1));
+            }
+       
             return (chosenNumbers/1.0/totalNumbers);
+
         }
     }
 }
