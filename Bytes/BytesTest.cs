e048f2b5c7f562e5e3b986b7e5c082729f7d5baa
minzatmarius <minzat.marius94@yahoo.com>
2016-01-17 14:08:48 +0200

    Bytes empty

---
+++ Bytes/BytesTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bytes
+{
+    [TestClass]
+    public class BytesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        byte[] ToBinary(int number)
+        {
+
+            return ;
+        }
+    }
+}
