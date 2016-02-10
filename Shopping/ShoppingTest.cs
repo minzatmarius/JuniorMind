247bcb9eeedb10ff746245f6ee24c8a9e8c0c9e7
minzatmarius <minzat.marius94@yahoo.com>
2016-02-10 13:37:40 +0200

    Created CalculateTotalPrice function

--- Shopping/ShoppingTest.cs
+++ Shopping/ShoppingTest.cs
@@ -4,11 +4,38 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Shopping
 {
     [TestClass]
-    public class UnitTest1
+    public class ShoppingTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TotalShouldBe9()
         {
+            Item[] items = new Item[] { new Item("Apple", 2), new Item("Milk", 4), new Item("Bread", 3) };
+            Assert.AreEqual(9, CalculateTotalPrice(items));
         }
+
+        struct Item
+        {
+            public string name;
+            public decimal price;
+
+            public Item(string name, decimal price)
+            {
+                this.name = name;
+                this.price = price;
+            }
+        }
+
+
+        decimal CalculateTotalPrice(Item[] items)
+        {
+            decimal total = 0;
+            for(int i = 0; i < items.Length; i++)
+            {
+                total += items[i].price;
+            }
+            return total;
+        }
+
+
     }
 }
