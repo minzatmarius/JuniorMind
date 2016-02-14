42a2cc0ce1a44b61674046a4b539067332ed29de
minzatmarius <minzat.marius94@yahoo.com>
2016-02-14 18:02:03 +0200

    Created Team and Game structures

---
+++ Fotbal/FotbalTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fotbal
+{
+    [TestClass]
+    public class FotbalTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        struct Game
+        {
+            Team teamA;
+            Team teamB;
+        }
+        struct Team
+        {
+            string name;
+            int goals;
+        }
+
+
+    }
+}
