86281d88bb6b2d5f7d01c6327879081bb5f46a9a
minzatmarius <minzat.marius94@yahoo.com>
2016-02-14 17:50:00 +0200

    GeneratePassword now generates uppercase letters, digits and symbols too

--- Password/PasswordTest.cs
+++ Password/PasswordTest.cs
@@ -12,6 +12,19 @@ namespace Password
             Password password = new Password(6, 6, 0, 0, 0);
             Assert.AreEqual(6, GeneratePassword(password).Length);
         }
+        [TestMethod]
+        public void GeneratePassword()
+        {
+            Password password = new Password(8, 2, 2, 2, 2);
+            // Assert.AreEqual(9, GeneratePassword(password).Length);
+            Assert.AreEqual("abcdefgh", GeneratePassword(password));
+        }
+        [TestMethod]
+        public void ShuffleTest()
+        {
+            string input = "aaBB22@#";
+            Assert.AreEqual(input, Shuffle(input));
+        }
 
         struct Password
         {
@@ -30,17 +43,57 @@ namespace Password
                 this.symbols = symbols;
             } 
         }
+
+
+        string Shuffle(string input)
+        {
+            string output = "";
+            int currentRandomPosition = 0;
+            Random position = new Random();
+            while (input.Length > output.Length) {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    currentRandomPosition = input[position.Next(0, input.Length)];
+                    output += input[currentRandomPosition];
+                
+    
+                }
+            }
+          
+            return output;
+        }
         string GeneratePassword(Password password)
         {
             string result = "";
             Random random = new Random();
+
             while (password.lowercase > 0)
             {
-                result += (char)random.Next(97, 123);
+                char nextCharacter = (char)random.Next(96, 123);
+                if (nextCharacter == 'l' || nextCharacter == 'o') continue;
+                result += nextCharacter;
                 password.lowercase--;
             }
-            return result;
 
+            while (password.uppercase > 0)
+            {
+                result += (char)random.Next(65, 91);
+                password.uppercase--;
+            }
+            
+            while (password.digits > 0)
+            {
+                result += (char)random.Next(50, 58);
+                password.digits--;
+            }
+
+            while(password.symbols > 0)
+            {
+                result += (char)random.Next(33, 48);
+                password.symbols--;
+            }
+
+            return result;
         }
     }
 }
