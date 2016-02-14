37964685c5bdc7d08f486bde6c38bc1e90f7d44f
minzatmarius <minzat.marius94@yahoo.com>
2016-02-14 14:16:24 +0200

    Created a function to generate the lowercase letters in a password

---
+++ Password/PasswordTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Password
+{
+    [TestClass]
+    public class PasswordTest
+    {
+        [TestMethod]
+        public void Generate6Lowercase()
+        {
+            Password password = new Password(6, 6, 0, 0, 0);
+            Assert.AreEqual(6, GeneratePassword(password).Length);
+        }
+
+        struct Password
+        {
+            public int length;
+            public int lowercase;
+            public int uppercase;
+            public int digits;
+            public int symbols;
+
+            public Password(int length, int lowercase, int uppercase, int digits, int symbols)
+            {
+                this.length = length;
+                this.lowercase = lowercase;
+                this.uppercase = uppercase;
+                this.digits = digits;
+                this.symbols = symbols;
+            } 
+        }
+        string GeneratePassword(Password password)
+        {
+            string result = "";
+            Random random = new Random();
+            while (password.lowercase > 0)
+            {
+                result += (char)random.Next(97, 123);
+                password.lowercase--;
+            }
+            return result;
+
+        }
+    }
+}
