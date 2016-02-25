8ae4c9e9b945281feab6bd8cdec1bb3bdc9f237f
minzatmarius <minzat.marius94@yahoo.com>
2016-02-25 16:07:44 +0200

    Created Records and Cyclist structures

--- Cyclemeter/CyclemeterTest.cs
+++ Cyclemeter/CyclemeterTest.cs
@@ -4,11 +4,41 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Cyclemeter
 {
     [TestClass]
-    public class UnitTest1
+    public class CyclemeterTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TotalDistanceForOneCyclist()
         {
+            Cyclist cyclist = new Cyclist(1, new Records[]  {new Records(1, 1), new Records(1, 2) });
+            Assert.AreEqual(3.14 * 1 * 1, GetDistance(cyclist));
         }
+
+       
+
+        struct Records
+        {
+            public int rotations;
+            public int second;
+
+            public Records(int rotations, int second)
+            {
+                this.rotations = rotations;
+                this.second = second;
+            }
+        }
+
+        struct Cyclist
+        {
+            public int diameter;
+            public Records[] records;
+            public Cyclist(int diameter, Records[] records)
+            {
+                this.diameter = diameter;
+                this.records = records;
+            }
+        }
+
+
+           
     }
 }
