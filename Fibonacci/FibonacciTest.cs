8aff7f692498c5f236a5ff45e685e1f493592cd6
minzatmarius <minzat.marius94@yahoo.com>
2016-03-03 18:06:34 +0200

    Fibonacci

---
+++ Fibonacci/FibonacciTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fibonacci
+{
+    [TestClass]
+    public class FibonacciTest
+    {
+        
+        [TestMethod]
+        public void FibonacciOn0()
+        {
+            Assert.AreEqual(0, GetFibonacci(0));
+        }
+
+        [TestMethod]
+        public void FibonacciOn1()
+        {
+            Assert.AreEqual(1, GetFibonacci(1));
+        }
+
+        [TestMethod]
+        public void FibonacciOn2()
+        {
+            Assert.AreEqual(1, GetFibonacci(2));
+        }
+
+        [TestMethod]
+        public void FibonacciOn3()
+        {
+            // 0 1 1 2 3 5 8 13
+            Assert.AreEqual(2, GetFibonacci(3));
+        }
+
+        [TestMethod]
+        public void FibonacciOn7()
+        {
+            Assert.AreEqual(13, GetFibonacci(7));
+        }
+
+        int GetFibonacci(int number)
+        {
+            return number < 2 ? number : (GetFibonacci(number - 1) + GetFibonacci(number - 2));
+            //if (number < 2) return number;
+            //return GetFibonacci(number - 1) + GetFibonacci(number - 2);
+        }
+    }
+}
