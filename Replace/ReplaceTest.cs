ba167b2b8866e0fa65ca30df8495d415394ab1da
minzatmarius <minzat.marius94@yahoo.com>
2016-03-03 19:02:20 +0200

    Replace empty

---
+++ Replace/ReplaceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Replace
+{
+    [TestClass]
+    public class ReplaceTest
+    {
+        [TestMethod]
+        public void ReplaceXwithICS()
+        {
+            string originalString = "axe";
+            char character = 'x';
+
+            Assert.AreEqual("aICSe", ReplaceCharacter(string original, char c));
+        }
+
+        ReplaceCharacter(string original, char c, string replaceWith = "ICS");
+    }
+}
