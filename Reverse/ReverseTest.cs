ff1ba96f7493884e24cdc80b7bc14e5479986a67
minzatmarius <minzat.marius94@yahoo.com>
2016-03-03 18:53:42 +0200

    Created ReverseString

---
+++ Reverse/ReverseTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reverse
+{
+    [TestClass]
+    public class ReverseTest
+    {
+        [TestMethod]
+        public void ReverseAB()
+        {
+            Assert.AreEqual("ba", ReverseString("ab"));
+        }
+
+        [TestMethod]
+        public void ReverseABCDE()
+        {
+            Assert.AreEqual("edcba", ReverseString("abcde"));
+        }
+
+        string ReverseString(string input)
+        {
+            string output = string.Empty;
+            if (input.Length < 2) return input;
+            
+            return input[input.Length - 1] + ReverseString(input.Remove(input.Length - 1));
+
+        }
+    }
+}
