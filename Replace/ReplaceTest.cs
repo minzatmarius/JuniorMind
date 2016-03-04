764f2cfde4f08b512869ab23ecc6ca0fb9554830
minzatmarius <minzat.marius94@yahoo.com>
2016-03-04 18:17:12 +0200

    ReplaceCharacter works

--- Replace/ReplaceTest.cs
+++ Replace/ReplaceTest.cs
@@ -12,17 +12,29 @@ namespace Replace
             string originalString = "axe";
             char character = 'x';
 
-            Assert.AreEqual("aICSe", ReplaceCharacter(originalString, character));
+            Assert.AreEqual("aICSe", ReplaceCharacter(originalString, character, "ICS"));
         }
 
-        string ReplaceCharacter(string original, char character, string replaceWith = "ICS")
+        [TestMethod]
+        public void ReplaceKWithKAPPA()
         {
-            if (original.Length < 2) return original;
-            if(original[original.Length - 1] != character)
+            string originalString = "akbkcdke";
+            Assert.AreEqual("aKAPPAbKAPPAcdKAPPAe", ReplaceCharacter(originalString, 'k', "KAPPA"));
+        }
+
+        string ReplaceCharacter(string original, char character, string replaceWith )
+        {
+            if (original.Length < 2)
             {
-                replaceWith = "";
+                return original;
             }
-            return replaceWith + ReplaceCharacter(original.Remove(original.Length - 1), character);
+            if (original[original.Length - 1] == character)
+            {
+                return ReplaceCharacter(original.Substring(0, original.Length -1), character, replaceWith)
+                      + replaceWith ;
+            }
+            return ReplaceCharacter(original.Substring(0, original.Length - 1), character, replaceWith)
+                      + original[original.Length - 1];
         }
     }
 }
