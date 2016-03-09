c6009fda60fd7209fb0111ca27f543dcb260e763
minzatmarius <minzat.marius94@yahoo.com>
2016-03-09 12:25:28 +0200

    Created GetLine function and tests passed for the first two lines

---
+++ Pascal/PascalTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pascal
+{
+    [TestClass]
+    public class PascalTest
+    {
+        [TestMethod]
+        public void FirstLineIs1()
+        {
+            int[] expected = { 1 };
+            CollectionAssert.AreEqual(expected, GetLine(1));
+        }
+
+        [TestMethod]
+        public void SecondLineIs11()
+        {
+            int[] expected = { 1, 1 };
+            CollectionAssert.AreEqual(expected, GetLine(2));
+        }
+
+        int[] GetLine(int lineNumber)
+        {
+            int[] elements = new int[lineNumber]; 
+            if(lineNumber < 3)
+            {
+                elements[0] = 1;
+                elements[lineNumber - 1] = 1;
+                return elements;
+            }
+
+            return elements;
+        }
+    }
+}
