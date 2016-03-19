1e7fb0b6655943bb4b25bfcdf4136394359e2db0
minzatmarius <minzat.marius94@yahoo.com>
2016-03-19 15:38:18 +0200

    Created Case enum

---
+++ RepairCenter/RepairCenterTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepairCenter
+{
+    [TestClass]
+    public class RepairCenterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        enum Case
+        {
+            Low = 1,
+            Medium = 2,
+            High = 3
+        }
+    }
+}
