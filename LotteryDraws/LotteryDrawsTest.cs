9d455dac11f342e118a4d03ec205598fe37d941e
minzatmarius <minzat.marius94@yahoo.com>
2016-03-19 15:03:29 +0200

    LotteryDraws empty

---
+++ LotteryDraws/LotteryDrawsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LotteryDraws
+{
+    [TestClass]
+    public class LotteryDrawsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5, 6 }, Sort(new int[] { 2, 3, 1, 4, 5, 6 }));
+        }
+
+        int[] Sort(int[] numbers)
+        {
+            return numbers;   
+        }
+    }
+}
