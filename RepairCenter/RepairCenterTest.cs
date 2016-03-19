8a0dfef49d79721dc7f9b6f40623cba15de72971
minzatmarius <minzat.marius94@yahoo.com>
2016-03-19 16:02:40 +0200

    Created Swap function

--- RepairCenter/RepairCenterTest.cs
+++ RepairCenter/RepairCenterTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RepairCenter
@@ -9,8 +10,27 @@ namespace RepairCenter
         [TestMethod]
         public void TestMethod1()
         {
+            Case[] cases = new Case[] { new Case("Case1", Priority.Medium),
+                                        new Case("Case2", Priority.Low),
+                                        new Case("Case3", Priority.High) };
+
+            Case[] expected = new Case[] { cases[2], cases[0], cases[1] };
+            CollectionAssert.AreEqual(expected, SortCases(cases));
 
         }
+
+        void Swap(ref Case a, ref Case b)
+        {
+            var aux = a;
+            a = b;
+            b = aux;
+        }
+
+        Case[] SortCases(Case[] cases)
+        {
+            
+        }
+
         public enum Priority
         {
             Low = 1,
