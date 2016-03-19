dbd3f8bd967cb6efc1fb1358333c3df5ef13d845
minzatmarius <minzat.marius94@yahoo.com>
2016-03-19 15:34:18 +0200

    Created BubbleSort function

--- LotteryDraws/LotteryDrawsTest.cs
+++ LotteryDraws/LotteryDrawsTest.cs
@@ -8,9 +8,15 @@ namespace LotteryDraws
     public class LotteryDrawsTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SortRandomArray()
         {
-            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5, 6 }, Sort(new int[] { 2, 3, 1, 4, 5, 6 }));
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5, 6 }, BubbleSort(new int[] { 2, 3, 1, 4, 5, 6 }));
+        }
+
+        [TestMethod]
+        public void SortReversedArray()
+        {
+            CollectionAssert.AreEqual(new int[] { 12, 21, 33, 40, 45, 46 }, BubbleSort(new int[] { 46, 45, 40, 33, 21, 12}));
         }
 
         void Swap(ref int a, ref int b)
@@ -20,8 +26,22 @@ namespace LotteryDraws
             b = aux;
         }
 
-        int[] Sort(int[] numbers)
+        int[] BubbleSort(int[] numbers)
         {
+            bool sorted = false;
+            while (!sorted)
+            {
+                sorted = true;
+                for(int i = 0; i< numbers.Length - 1; i++)
+                {
+                    if(numbers[i] > numbers[i + 1])
+                    {
+                        Swap(ref numbers[i], ref numbers[i + 1]);
+                        sorted = false;
+                    }
+                }
+            }
+
             return numbers;   
         }
     }
