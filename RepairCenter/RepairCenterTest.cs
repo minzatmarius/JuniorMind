0e245c126cd8f617c5c09418dd9b109a33540a90
minzatmarius <minzat.marius94@yahoo.com>
2016-03-19 15:48:30 +0200

    Renamed the enum Priority and used it in struct Case

--- RepairCenter/RepairCenterTest.cs
+++ RepairCenter/RepairCenterTest.cs
@@ -9,13 +9,24 @@ namespace RepairCenter
         [TestMethod]
         public void TestMethod1()
         {
-        }
 
-        enum Case
+        }
+        public enum Priority
         {
             Low = 1,
             Medium = 2,
             High = 3
+        }
+        struct Case
+        {
+            public string name;
+            Priority priority;
+
+            public Case(string name, Priority priority)
+            {
+                this.name = name;
+                this.priority = priority;
+            }
         }
     }
 }
