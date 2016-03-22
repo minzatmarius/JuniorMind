941d2c53290773451251aa39801cd53a3982b1bb
minzatmarius <minzat.marius94@yahoo.com>
2016-03-22 12:04:07 +0200

    Created OrderWords function based on Insertion and removed GetWords

--- OrderedWords/OrderedWordsTest.cs
+++ OrderedWords/OrderedWordsTest.cs
@@ -13,37 +13,56 @@ namespace OrderedWords
             Assert.AreEqual(3, Count("word", input));
         }
 
+
+        [TestMethod]
+        public void OrderThreeWords()
+        {
+            string input = "word word word2";
+            Assert.AreEqual("word2 word word", OrderWords(input));
+        }
+
+        [TestMethod]
+        public void OrderMoreWords()
+        {
+            string input = "word2 word3 word1 word3 word2 word3";
+            Assert.AreEqual("word1 word2 word2 word3 word3 word3", OrderWords(input));
+        }
+        void Swap(ref string a, ref string b)
+        {
+            var aux = a;
+            a = b;
+            b = aux;
+        }
+
         int Count(string word, string input)
         {
             int count = 0;
             string[] words = input.Split(' ');
-            for(int i = 0; i < words.Length; i++)
+            for (int i = 0; i < words.Length; i++)
             {
                 if (words[i] == word) count++;
             }
             return count;
         }
 
-        [TestMethod]
-        public void CreateNewStringWithUniqueWords()
-        {
-            string input = "word word word2";
-            Assert.AreEqual("word word2 ", GetWords(input));
-        }
 
-        string GetWords(string input)
+
+        string OrderWords(string input)
         {
+
             string[] words = input.Split(' ');
-            string uniqueWords = "";
-            for(int i = 0; i < words.Length; i++)
+            for (int i = 1; i < words.Length; i++)
             {
-                if (!uniqueWords.Contains(words[i]))
+                for (int j = i; j > 0; j--)
                 {
-                    uniqueWords += words[i] + " ";
+                    if (Count(words[j], input) < Count(words[j - 1], input))
+                    {
+                        Swap(ref words[j], ref words[j - 1]);
+                    }
                 }
             }
 
-            return uniqueWords;
+            return string.Join(" ", words);
         }
     }
 }
