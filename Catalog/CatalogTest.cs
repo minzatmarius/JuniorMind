5a8f81285febb9fd621dd4f2796c1a0c8ee1a396
minzatmarius <minzat.marius94@yahoo.com>
2016-03-26 16:48:35 +0200

    Created Student and Subject structures

---
+++ Catalog/CatalogTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catalog
+{
+    [TestClass]
+    public class CatalogTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        struct Student
+        {
+            public string name;
+            public Subject[] subjects;
+            public Student(string name, Subject[] subjects)
+            {
+                this.name = name;
+                this.subjects = subjects;
+            }
+        }
+        struct Subject
+        {
+            public int[] marks;
+            public Subject(int[] marks)
+            {
+                this.marks = marks;
+            }
+        }
+    }
+}
