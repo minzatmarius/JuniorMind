61baca78b3d7ace6c0f5e8fc1e5c28720ace3a9d
minzatmarius <minzat.marius94@yahoo.com>
2016-04-06 21:06:17 +0300

    Same content

--- Vector/Vector/Vector.cs
+++ Vector/Vector/Vector.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,165 @@ using System.Threading.Tasks;
 
 namespace Vector
 {
-    class Vector
+    class Vector<T> : IList<T>
     {
+
+        private T[] content;
+
+        public Vector()
+        {
+            content = new T[0];
+        }
+
+
+        public Vector(T[] content)
+        {
+            this.content = new T[content.Length];
+            for (int i = 0; i < content.Length; i++)
+                this.content[i] = content[i];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return content[index];
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < content.Length; i++)
+                {
+                    if (content[i] != null) count++;
+                }
+                return count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Add(T item)
+        {
+
+            if (content.Length == Count)
+                Array.Resize(ref content, content.Length * 2);
+
+            if (content.Length == 0)
+                Array.Resize(ref content, content.Length + 1);
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                if (content[i].Equals(default(T)))
+                {
+                    content[i] = item;
+                    break;
+                }
+            }
+
+
+        }
+
+
+        public void Clear()
+        {
+            content = new T[0];
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < content.Length; i++)
+            {
+                if (content[i].Equals(item)) return true;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            int length = array.Length;
+            Array.Resize<T>(ref array, array.Length + content.Length);
+            for (int i = arrayIndex + content.Length; i < array.Length; i++)
+            {
+                array[i] = array[i - content.Length];
+            }
+            for (int i = arrayIndex; i < content.Length; i++)
+            {
+                array[i] = content[i - arrayIndex];
+            }
+
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (IEnumerator<T>)content.GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < content.Length; i++)
+            {
+
+                if (content[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+
+
+        }
+
+        public void Insert(int index, T item)
+        {
+            Array.Resize<T>(ref content, content.Length + 1);
+            for (int i = content.Length - 1; i > index; i--)
+            {
+                content[i] = content[i - 1];
+            }
+            content[index] = item;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            T[] contentCopy = content;
+            if (index == -1) return false;
+            RemoveAt(index);
+
+            return (content == contentCopy) ? false : true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < content.Length - 1; i++)
+            {
+                content[i] = content[i + 1];
+            }
+
+            Array.Resize<T>(ref content, content.Length - 1);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+
+        }
     }
 }
--- Vector/Vector/VectorTest.cs
+++ Vector/Vector/VectorTest.cs
@@ -1,14 +1,102 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
 namespace Vector
 {
     [TestClass]
-    public class UnitTest1
+    public class VectorTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCount()
         {
+            Vector<int> obj = new Vector<int>(new int[] { 1, 2, 3 });
+            obj.RemoveAt(2);
+            Assert.AreEqual(2, obj.Count);
+
         }
+
+        [TestMethod]
+        public void TestClear()
+        {
+            Vector<int> obj = new Vector<int>(new int[] { 1, 2, 3 });
+            obj.Clear();
+            Assert.AreEqual(0, obj.Count);
+        }
+
+        [TestMethod]
+        public void TestReadOnly()
+        {
+            Vector<int> obj = new Vector<int>(new int[] { 1, 2, 3 });
+            Assert.IsFalse(obj.IsReadOnly);
+        }
+        [TestMethod]
+        public void TestAdd()
+        {
+            Vector<int> obj = new Vector<int>();
+            obj.Add(1);
+            Assert.IsTrue(obj.Contains(1));
+
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            Vector<int> obj = new Vector<int>(new int[] { 1, 2, 3 });
+            Assert.IsTrue(obj.Contains(2));
+        }
+
+        [TestMethod]
+        public void TestContainsNot()
+        {
+            Vector<int> obj = new Vector<int>(new int[] { 1, 2, 3 });
+            Assert.IsFalse(obj.Contains(4));
+        }
+
+        [TestMethod]
+        public void TestIndexOf()
+        {
+            Vector<int> obj = new Vector<int>(new int[] { 1, 2, 3 });
+            Assert.AreEqual(1, obj.IndexOf(2));
+            Assert.AreEqual(-1, obj.IndexOf(5));
+        }
+
+        [TestMethod]
+        public void TestInsert()
+        {
+            Vector<int> obj = new Vector<int>(new int[] { 1, 2, 3 });
+            obj.Insert(0, 5);
+            Assert.AreEqual(0, obj.IndexOf(5));
+            Assert.AreEqual(1, obj.IndexOf(1));
+            Assert.AreEqual(3, obj.IndexOf(3));
+        }
+
+        [TestMethod]
+        public void TestRemoveAt()
+        {
+            Vector<int> obj = new Vector<int>(new int[] { 1, 2, 3 });
+            obj.RemoveAt(0);
+            Assert.AreEqual(0, obj.IndexOf(2));
+            Assert.IsFalse(obj.Contains(1));
+        }
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            Vector<int> obj = new Vector<int>(new int[] { 1, 2, 3 });
+            Assert.IsTrue(obj.Remove(2));
+            Assert.IsFalse(obj.Remove(4));
+        }
+
+        [TestMethod]
+        public void TestCopyTo()
+        {
+            Vector<int> obj = new Vector<int>(new int[] { 1, 2, 3 });
+            int[] array = { 7, 8 };
+            obj.CopyTo(array, 1);
+            CollectionAssert.AreEqual(new int[] { 7, 1, 2, 3, 8 }, array);
+
+        }
+
     }
 }
