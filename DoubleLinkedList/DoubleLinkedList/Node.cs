3998c0fec30f52855f540a8144dd39cabd30ec10
minzatmarius <minzat.marius94@yahoo.com>
2016-04-11 19:53:25 +0300

    Created Node class

---
+++ DoubleLinkedList/DoubleLinkedList/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleLinkedList
+{
+    class Node
+    {
+        private object data;
+        Node next;
+
+        public Node(object data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+}
